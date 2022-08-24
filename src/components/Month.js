d752c865f8c15a6a8ac823514be42a4003c7fec8
Yoshi <yoshimejia00@gmail.com>
2022-08-24 13:16:59 -0400

    added div and class

--- src/components/Month.js
+++ src/components/Month.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const Month = () => {
     return(
-        
-        <h1>August</h1>
+        <div className="month">
+            <h1>August</h1>
+        </div>
     )
 }
 
