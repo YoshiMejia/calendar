cb22e82aad100e2692212766fdf3e3fd125a258d
Yoshi <yoshimejia00@gmail.com>
2022-08-23 20:26:39 -0400

    new file: renders title & year

---
+++ src/components/Banner.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Banner = () => {
+
+    return(
+        <>
+        <div className="title">
+            <h1>My Calendar</h1>
+        </div>
+
+        <div className="year">
+            <h1>
+                2022
+            </h1>
+        </div>
+        </>
+    )
+    
+}
+
+export default Banner
