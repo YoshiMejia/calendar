1e5eda6997d142dce7b5f385053cbae2d0803b58
Yoshi <yoshimejia00@gmail.com>
2022-08-24 14:27:23 -0400

    arrow component first commit

---
+++ src/components/Arrows.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Arrows = () => {
+
+    const left = require('../media/left.png');
+    const right = require('../media/right.webp');
+    return(
+        <div className="arrows">
+            <img src={left} alt="left arrow" className="left arrow"></img>
+            <img src={right} alt="right arrow" className="right arrow"></img>
+        </div>
+    )
+}
+
+export default Arrows
