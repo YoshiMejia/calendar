f8a3fc8fcb861ea184543dc1a74f52dc10b98c49
Yoshi <yoshimejia00@gmail.com>
2022-08-24 13:22:59 -0400

    added extra months

--- src/components/Month.js
+++ src/components/Month.js
@@ -3,7 +3,9 @@ import React from "react";
 const Month = () => {
     return(
         <div className="month">
+            <h3>July</h3>
             <h1>August</h1>
+            <h3>September</h3>
         </div>
     )
 }
