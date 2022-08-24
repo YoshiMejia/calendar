a7c069dae912649670cbe8f0082f4b6fdcf1b82b
Yoshi <yoshimejia00@gmail.com>
2022-08-24 13:33:04 -0400

    added div for weekdays

--- src/containers/MonthView.js
+++ src/containers/MonthView.js
@@ -5,15 +5,17 @@ import Banner from "../components/Banner";
 const MonthView = () => {
     return(
         <div className="monthview">
-        <Banner />
-        <Month/>
-            <h2>Sunday</h2>
-            <h2>Monday</h2>
-            <h2>Tuesday</h2>
-            <h2>Wednesday</h2>
-            <h2>Thursday</h2>
-            <h2>Friday</h2>
-            <h2>Saturday</h2>
+            <Banner />
+            <Month/>
+                <div className="weekdays">
+                    <h2>Sunday</h2>
+                    <h2>Monday</h2>
+                    <h2>Tuesday</h2>
+                    <h2>Wednesday</h2>
+                    <h2>Thursday</h2>
+                    <h2>Friday</h2>
+                    <h2>Saturday</h2>
+                </div>
         </div>
     )
 }
