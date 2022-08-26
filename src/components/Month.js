071a51939436bbddb424398eaf3550a135393a3a
Yoshi <yoshimejia00@gmail.com>
2022-08-26 12:57:49 -0400

    changing months to accomodate for project submission date

--- src/components/Month.js
+++ src/components/Month.js
@@ -3,9 +3,9 @@ import React from "react";
 const Month = () => {
     return(
         <div className="month">
-            <h3>July</h3>
-            <h1>August</h1>
-            <h3>September</h3>
+            <h3>August</h3>
+            <h1>September</h1>
+            <h3>October</h3>
         </div>
     )
 }
