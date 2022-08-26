dff9ddb343f158c8649b007c5e90e8ef888501a8
Yoshi <yoshimejia00@gmail.com>
2022-08-25 21:46:07 -0400

    added signin button

--- src/containers/Events.js
+++ src/containers/Events.js
@@ -3,7 +3,7 @@ import React from "react";
 const Events = () => {
     return (
         <div className="event-cont">
-
+        <a href="#blank" className="signin" onClick={e => {e.preventDefault()}}>Sign-In</a>
         </div>
     )
 }
