4f571e533e76808016013695a660b8d6d1c1777c
Yoshi <yoshimejia00@gmail.com>
2022-08-25 21:51:00 -0400

    adding events to calendar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import MonthView from './containers/MonthView';
+import Events from './containers/Events';
 
 function App() {
   return (
     <div className="App">
       <MonthView/>
+      <Events />
     </div>
   );
 }
