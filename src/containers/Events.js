0bca87fac6608805219ec09411f2275824627e6c
Yoshi <yoshimejia00@gmail.com>
2022-08-27 19:58:40 -0400

    added add event button

--- src/containers/Events.js
+++ src/containers/Events.js
@@ -3,7 +3,9 @@ import React from "react";
 const Events = () => {
     return (
         <div className="event-cont">
-        <a href="#blank" className="signin" onClick={e => {e.preventDefault()}}>Sign-In</a>
+            <a href="#blank" className="signin" onClick={e => {e.preventDefault()}}>Sign-In</a>
+            <a href="#blank" className="addevent" onClick={e => {e.preventDefault()}}>Add An Event</a>
+
         </div>
     )
 }
