4c623599136112c5717192d94876d3c2827f8dbc
Yoshi <yoshimejia00@gmail.com>
2022-08-27 20:00:08 -0400

    added divs for each button

--- src/containers/Events.js
+++ src/containers/Events.js
@@ -3,9 +3,13 @@ import React from "react";
 const Events = () => {
     return (
         <div className="event-cont">
-            <a href="#blank" className="signin" onClick={e => {e.preventDefault()}}>Sign-In</a>
-            <a href="#blank" className="addevent" onClick={e => {e.preventDefault()}}>Add An Event</a>
+            <div className="signin">
+                <a href="#blank" className="signin-btn" onClick={e => {e.preventDefault()}}>Sign-In</a>
+            </div>
 
+            <div className="addevent">
+                <a href="#blank" className="addevent-btn" onClick={e => {e.preventDefault()}}>Add An Event</a>
+            </div>
         </div>
     )
 }
