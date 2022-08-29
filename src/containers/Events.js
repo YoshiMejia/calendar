1b0f52d3be0c13d4609ffae680d3bc9f68c36d6b
Yoshi <yoshimejia00@gmail.com>
2022-08-29 19:47:58 -0400

    importing Today component

--- src/containers/Events.js
+++ src/containers/Events.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Today from "../components/events/Today";
 
 const Events = () => {
     return (
@@ -7,6 +8,8 @@ const Events = () => {
                 <a href="#blank" className="signin-btn clr-bck" onClick={e => {e.preventDefault()}}>Sign-In</a>
             </div>
 
+            <Today />
+            
             <div className="addevent clr-bck">
                 <a href="#blank" className="addevent-btn clr-bck" onClick={e => {e.preventDefault()}}>Add An Event</a>
                 <p className="plus clr-bck">+</p>
