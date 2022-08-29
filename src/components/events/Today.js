404f0e746cc7d31fffb5365d25aed332c55cc835
Yoshi <yoshimejia00@gmail.com>
2022-08-29 19:47:49 -0400

    switch statement to return today's weekday name

--- src/components/events/Today.js
+++ src/components/events/Today.js
@@ -2,9 +2,38 @@ import React from "react";
 
 const Today = () => {
 
+    const d = new Date();
+    let day = d.getDay();
+
+    switch (day) {
+        case 0:
+          day = "Sunday";
+          break;
+        case 1:
+          day = "Monday";
+          break;
+        case 2:
+           day = "Tuesday";
+          break;
+        case 3:
+          day = "Wednesday";
+          break;
+        case 4:
+          day = "Thursday";
+          break;
+        case 5:
+          day = "Friday";
+          break;
+        case 6:
+          day = "Saturday";
+          break;
+        default:
+        day = "Doomsday";
+      }
+
     return(
         <div className="today">
-
+            {day}
         </div>
     )
 }
