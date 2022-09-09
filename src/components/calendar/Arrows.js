59f7c56b5a57aa816a3eaeb7931b74398b92d3d8
Yoshi <yoshimejia00@gmail.com>
2022-09-09 17:02:19 -0400

    commenting out: switching from imgs to text

--- src/components/calendar/Arrows.js
+++ src/components/calendar/Arrows.js
@@ -2,12 +2,18 @@ import React from "react";
 
 const Arrows = () => {
 
-    const left = require('../../media/left.png');
-    const right = require('../../media/right.webp');
+    // const left = require('../../media/left.png');
+    // const right = require('../../media/right.webp');
     return(
         <div className="arrows">
-            <img src={left} alt="left arrow" className="left arrow"></img>
-            <img src={right} alt="right arrow" className="right arrow"></img>
+            {/* <img src={left} alt="left arrow" className="left arrow"></img>
+            <img src={right} alt="right arrow" className="right arrow"></img> */}
+            <p className="left arrow">
+                &lt;
+            </p>
+            <p className="right arrow">
+                &gt;
+            </p>
         </div>
     )
 }
