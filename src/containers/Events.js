2a38aff477d24fea9293200b80463065766a39c2
Yoshi <yoshimejia00@gmail.com>
2022-09-10 20:43:28 -0400

    changing from functional comp to class comp

--- src/containers/Events.js
+++ src/containers/Events.js
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import Today from "../components/events/Today";
 
-const Events = () => {
-    return (
-        <div className="event-cont">
-            <div className="signin clr-bck">
-                <a href="#blank" className="signin-btn clr-bck" onClick={e => {e.preventDefault()}}>Sign-In</a>
+class Events extends Component {
+    
+    render(){
+        return (
+            <div className="event-cont">
+                <div className="signin clr-bck">
+                    <a href="#blank" className="signin-btn clr-bck" onClick={e => {e.preventDefault()}}>Sign-In</a>
+                </div>
+    
+                <Today />
+                
+                <div className="addevent clr-bck">
+                    <a href="#blank" className="addevent-btn clr-bck" onClick={e => {e.preventDefault()}}>Add An Event</a>
+                    <a href="#blank" className="plus clr-bck" onClick={e => {e.preventDefault()}}>+</a>
+                </div>
             </div>
-
-            <Today />
-            
-            <div className="addevent clr-bck">
-                <a href="#blank" className="addevent-btn clr-bck" onClick={e => {e.preventDefault()}}>Add An Event</a>
-                <p className="plus clr-bck">+</p>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Events
