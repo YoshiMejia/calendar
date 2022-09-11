33333e13488a1d197b9ed4c716b9a75285d034df
Yoshi <yoshimejia00@gmail.com>
2022-09-11 19:19:04 -0400

    importing right and left arrows

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import MonthView from './containers/MonthView';
 import Events from './containers/Events';
+import RightArrow from './components/arrows/RightArrow';
+import LeftArrow from './components/arrows/LeftArrow';
 
 function App() {
   return (
     <div className="App">
+      <LeftArrow/>
       <MonthView/>
+      <RightArrow/>
       <Events />
     </div>
   );
