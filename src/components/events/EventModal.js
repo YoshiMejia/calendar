18905fccd5b5570ce3036b66f9a0461cecf2f7e8
Yoshi <yoshimejia00@gmail.com>
2022-09-10 20:59:38 -0400

    event modal component first commit

---
+++ src/components/events/EventModal.js
@@ -0,0 +1,16 @@
+const EventModal = ({ handleClose, show, children }) => {
+    const showHideClassName = show ? "modal display-block" : "modal display-none";
+  
+    return (
+      <div className={showHideClassName}>
+        <section className="modal-main">
+          {children}
+          <button type="button" onClick={handleClose}>
+            Close
+          </button>
+        </section>
+      </div>
+    );
+  };
+
+export default EventModal
