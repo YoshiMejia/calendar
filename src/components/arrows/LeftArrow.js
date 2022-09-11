1d023fb5132667ccfaa3819a1dd53508fef1fde7
Yoshi <yoshimejia00@gmail.com>
2022-09-11 19:19:16 -0400

    creating leftarrow sole component

---
+++ src/components/arrows/LeftArrow.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LeftArrow = () => {
+    return(
+        <div className="left-arrow">
+            <div className="left-cont">
+                <a href="#blank" className="left arrow">
+                    &lt;
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default LeftArrow
